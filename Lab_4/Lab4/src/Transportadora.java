ff9b7c96c2a0918663a3dea992502f85141bd52f
Rafael Martins <rafael.rmf2020@gmail.com>
2026-05-16 17:34:49 -0300

    ADD: Lab4 POO

---
+++ Lab_4/Lab4/src/Transportadora.java
@@ -0,0 +1,7 @@
+public class Transportadora {
+
+    public static void transportar(Transportavel transportavel, String endereco) {
+        System.out.println("Transportando item de peso " + transportavel.getPeso() + " kg para o endereço: " + endereco);
+    }
+}
+
